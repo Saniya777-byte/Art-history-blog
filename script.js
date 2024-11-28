0049f4bc8912319c46095054dbec8e028885b58a
x-SUKUNA-x <sk4272916@gmail.com>
2024-11-28 10:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    const productGrid = document.querySelector(".product-grid");
-    const scrollLeft = document.querySelector(".scroll-button.left");
-    const scrollRight = document.querySelector(".scroll-button.right");
-
-    // Scroll left button
-    scrollLeft.addEventListener("click", () => {
-        productGrid.scrollBy({ left: -300, behavior: "smooth" });
-    });
-
-    // Scroll right button
-    scrollRight.addEventListener("click", () => {
-        productGrid.scrollBy({ left: 300, behavior: "smooth" });
-    });
-});
